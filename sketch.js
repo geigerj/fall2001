d4cdbd579eb5b508e03beb5f97b74440f2afe8c0
geigerj <jacgeiger@gmail.com>
2020-11-02 23:31:08 -0500

    updates
    * WebGL context loss error seems to be resolved by sending graphics
      texture instead of pixel array
    * Use `loadStrings` instead of `fetch`
    * Slow down scroll 20%

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ let pos = 0;
 let displacementMag = 0;
 let last_played = 0;
 let poems = [];
-let notes = [{}]
+let notes = [{}];
 let clicked = false;
 
 const soundtrack = {
@@ -53,7 +53,7 @@ async function preload(){
       }
     }
   )
-  
+
   notes.push(loadSound('media/C.mp3')); //1
   notes.push(loadSound('media/D.mp3')); //2
   notes.push(loadSound('media/E.mp3')); //3
@@ -65,55 +65,68 @@ async function preload(){
   notes.push(loadSound('media/B.mp3')); //9
   notes.push(loadSound('media/C2.mp3')); //10
   water.size(window.innerWidth, window.innerHeight);
-  poems = await loadPoems();
+  poems = loadStrings('poems.txt')
 }
 
 function setup() {
-  // See https://www.khronos.org/webgl/wiki/HandlingContextLost
-  // TODO: WebGL still crashes often... recovery isn't working?
-  var canvas = document.getElementById("defaultCanvas0");
-  canvas.addEventListener("webglcontextlost", function(event) {
-    event.preventDefault();
-}, false);
-  canvas.addEventListener(
-    "webglcontextrestored", function() {preload(); setup();}, false);
-    
+  poems = poems
+    .reduce(
+      (a, b) => {
+        b === "<><><>>>>>>>><<<<<<<<><><>"
+          ? a.push("")
+          : a.push(a.pop() + "\n" + b);
+        return a;
+      },
+      [""])
+    // randomize order
+    .map((a) => ({sort: Math.random(), value: a}))
+    .sort((a, b) => a.sort - b.sort)
+    .map((a) => a.value);
 
   background(0);
   createCanvas(window.innerWidth, window.innerHeight, WEBGL);
-  
-  img_graphics = createGraphics(window.innerWidth, window.innerHeight * 4);
 
+  img_graphics = createGraphics(window.innerWidth, window.innerHeight * 4);
   img_graphics.fill(255);
   img_graphics.textSize(70);
   img_graphics.textFont("Arial");
   img_graphics.textAlign(CENTER);
   img_graphics.noStroke();
   img_graphics.text('fall 2001', 0.3 * window.innerWidth, 0.8 * window.innerHeight);
-  
   img_graphics.textSize(18);
   img_graphics.text('click, then scroll to begin', 0.3 * window.innerWidth, 0.9 * window.innerHeight);
-  
-  
+
+  tmp_graphics = createGraphics(window.innerWidth, window.innerHeight);
+
   pixelDensity(1);
   frameRate(30);
 }
 
 function draw() {
-  
   displacementMag *= 0.985;
-  
   scaled_pos = pos % 620;
-  
+
   shader(waterShader);
-  waterShader.setUniform("horror", img_graphics.get(100*sin(pos * 0.06),scaled_pos + 100*sin(pos * 0.08),window.innerWidth, window.innerHeight));
+
+  // Passing output of get() directly to shader as a texture causes
+  // a memory leak (why?), so draw to an intermediate graphics first.
+  tmp_graphics.background(0);
+  tmp_graphics.image(img_graphics.get(
+    100*sin(pos * 0.06),
+    scaled_pos + 100*sin(pos * 0.08),
+    window.innerWidth,
+    window.innerHeight), 0, 0);
+
+  waterShader.setUniform("horror", tmp_graphics);
   waterShader.setUniform("displacement", water);
   waterShader.setUniform('time', pos);
   waterShader.setUniform('displacementMult', displacementMag * (scaled_pos * scaled_pos + 1000) * 0.0003);
-  
+
   rect(0,0,width,height);
 }
 
+// The audio won't start until mousePressed, so don't start anything
+// until then
 function mousePressed() {
   clicked = true;
 }
@@ -121,33 +134,27 @@ function mousePressed() {
 function mouseWheel(event) {
   if (event.delta < 0 || !clicked)
     return;
-  pos += event.delta / 4;
+
+  pos += event.delta / 5;
   displacementMag = 1;
+  // New poem every 620 px
   if (pos - last_fetch_pos > 620) {
     fetchPoem();
     last_fetch_pos = pos;
   }
+  // New soundtrack beat every 130px
   if (pos - last_play_pos > 130) {
     playNext();
     last_play_pos = pos;
   }
 }
 
-async function loadPoems() {
-    const response = await fetch('poems.txt');
-    const data = await response.text();
-    return data.split("<><><>>>>>>>><<<<<<<<><><>")
-      .map((a) => ({sort: Math.random(), value: a}))
-      .sort((a, b) => a.sort - b.sort)
-      .map((a) => a.value);
-}
-
 function fetchPoem() {
   img_graphics.background(0);
   let txt = poems.pop();
-  
+
   img_graphics.textSize(20);
-  
+
   img_graphics.textAlign(LEFT);
   img_graphics.text(txt, 0.6 * window.innerWidth, 0.8 * window.innerHeight);
 }
